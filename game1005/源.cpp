52ad20aa3efcb40b822633dfc512a4e65e085e5e
yangs <yangs@DESKTOP-OO6INOV>
2023-01-22 18:06:50 -0600

    添加项目文件。

---
+++ game1005/源.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+#include <string>
+
+int main() {
+
+	/*int a = 0;
+
+	cin >> a;
+
+	double f = 3.14;
+
+	cin >> f;
+
+	char ch = 'a';
+
+	cin >> ch;
+
+	string str = "hello";
+
+	cout << "input str" << endl;
+
+	cin >> str;
+
+	bool flag = true;
+
+	cin >> flag;		*/
+
+
+	//int a1 = 10;
+	//int b1 = 3;
+	//cout << a1 + b1 << endl;
+	//cout << a1 - b1 << endl;
+	//cout << a1 * b1 << endl;
+	//cout << (double)a1 / b1 << endl;
+
+	// 前置递增
+	//int a = 10;
+	//++a; // a + 1
+	//cout << a << endl;
+	//后置递增
+	//int b = 10;
+	//b++;
+	//cout << b << endl;
+
+	//int a2 = 10;
+	//int b2 = ++a2 *10;
+	
+	//int a = 5;
+	//int b = 5;
+	//a  += b;
+	//cout << a << endl;
+	//int a = 10;
+	//cout << !a << endl;
+	int a = 10;
+	int b = 10;
+	cout << (a && b) << endl;d
+   
+	
+	system("pause");
+
+	return 0;
+
+}
