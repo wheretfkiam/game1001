9ac77080fb59f4bb14d82513d5eaa6c03efc0a9a
wheretfkiam <122551580+wheretfkiam@users.noreply.github.com>
2023-01-26 16:44:23 -0600

    testing condition

---
+++ Game1001/switch语句.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "请打分" << endl;
+	int score = 0;
+	cin >> score;
+	cout << "分数： " << score << endl;
+
+	switch (score)
+	{
+		case 10: cout << "经典" << endl;
+			break;
+		case 9:cout << "经典" << endl;
+			break;
+		case 8: cout << "不错" << endl;
+			break;
+		case 7: cout
+	}
+	
+	
+	
+system("pause");
+return 0;
+}
---
+++ Game1001/源.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+/*int a = 10;
+	a = 100;
+	int b = 10;
+	a += 2;
+	cout << a << b << endl; */
+	
+	//int a = 10;
+	//a %= 2;
+
+	//条件语句
+	//1 读取用户输入2 打印用户输入 3 判断分数大小
+	/*int score = 0;
+	cout << "enter the score" << endl;
+	cin >> score;
+	if (score > 600) {
+
+		cout << "nice" << endl;
+	
+	}*/
+	//int score = 0;
+	//cout << "enter score" << endl;
+	//cin >> score;
+	//cout << "score: " << score << endl;
+	//if (score > 600)
+	//{
+	//	cout << "A" << endl;
+	//}
+	//else if (score > 500)
+	//{
+	//	cout << "B" << endl;
+	//}
+	//else if (score > 400)
+	//{
+	//	cout << "C" << endl;
+	//}
+	//else
+	//{
+	//	cout << "F" << endl;
+	//}
+	/*int score = 0;
+		
+	cout << "enter score" << endl;
+	cin >> score;
+	if (score > 600)
+	{
+		cout << "A" << endl;
+	}
+	else if (score > 500)
+	{
+		cout << "B" << endl;
+	}
+	else if (score > 400)
+	{
+		cout << "C" << endl;
+
+	}
+	else
+	{
+		cout << "F" << endl;
+	}*/
+
+
+	int a = 10;
+	int b = 20;
+	int c =  0;
+	 //c= (a > b ? a : b);
+
+	
+
+
+
+	system("pause");
+	return 0;
+}
